e175d21aa8b4dcd15a055b0aeedc24a59907174a
Dean Laurea <deanlaurea@gmail.com>
2015-05-15 00:43:22 -0500

    Arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,47 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    
+    return [characterString componentsSeparatedByString:@";"];
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+   
+    return [characterArray componentsJoinedByString:@";"];
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *alphabetization = [NSMutableArray arrayWithArray:characterArray];
+    NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:nil];
+    [alphabetization sortUsingDescriptors:@[sort]];
+    
+    return alphabetization;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
-    return NO;
+    
+    BOOL foundWorf = NO;
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[C] 'worf'"];
+    NSArray *worf = [characterArray filteredArrayUsingPredicate:containsWorf];
+    
+    if (worf.count > 0) {
+        foundWorf = YES;
+    }
+    
+    return foundWorf;
+
+/*    for (NSString *worf in characterArray) {
+        NSString *name = worf.lowercaseString;
+        NSRange worfRange = [name rangeOfString:@"worf"];
+        
+        if (worfRange.location != NSNotFound) {
+            return YES;
+        }
+    }
+    
+    return NO; */
 }
 
 @end
