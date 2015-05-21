4e0a8657f5032517e9bcd6f1a12e538a5c372b06
Dean Laurea <deanlaurea@gmail.com>
2015-05-21 05:39:14 -0500

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,52 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    return @([number intValue] * 2);
+    
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    
+    if (otherNumber == number) {
+        return @[@(number)];
+    }
+    
+    NSInteger lowNumber, highNumber;
+    
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    
+    }
+    
+    NSMutableArray  *returnArray = [NSMutableArray array];
+    
+    for (NSInteger i =lowNumber; i <=highNumber; i++) {
+        [returnArray addObject:@(i)];
+    }
+    
+    return returnArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    
+    NSInteger   lowestNumber = NSIntegerMax;
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        NSInteger integerValue = [number integerValue];
+        
+        if (integerValue < lowestNumber) {
+            lowestNumber = integerValue;
+        
+        }
+        
+    }
+    
+    return lowestNumber;
 }
 
 @end
